569122414c2ed2f89e04336676bdb21232af584d
Siddarth Banerjee <siddarth2801@gmail.com>
2025-05-06 18:47:37 +0530

    navlink component setup

--- src/components/navigation/navbar/MobileNavigation.jsx
+++ src/components/navigation/navbar/MobileNavigation.jsx
@@ -11,54 +11,13 @@ import {
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { sidebarLinks } from '@/constants';
-import { usePathname } from 'next/navigation';
 import ROUTES from '@/constants/routes';
-import { useSession } from 'next-auth/react';
-import { useState } from 'react';
-
-const NavContent = () => {
-  const pathname = usePathname();
-
-  return (
-    <section className="flex h-full flex-col gap-6">
-      {sidebarLinks.map(item => {
-        const isActive =
-          (pathname.includes(item.route) && item.route.length > 1) ||
-          pathname === item.route;
-
-        return (
-          <SheetClose asChild key={item.route}>
-            <Link
-              href={item.route}
-              className={`${
-                isActive
-                  ? 'primary-gradient rounded-lg text-light-900'
-                  : 'text-dark300_light900'
-              } flex items-center justify-start gap-4 bg-transparent p-4`}
-            >
-              <Image
-                src={item.imgURL}
-                alt={item.label}
-                width={20}
-                height={20}
-                className={`${isActive ? '' : 'invert-colors'}`}
-              />
-              <p className={`${isActive ? 'base-bold' : 'base-medium'}`}>
-                {item.label}
-              </p>
-            </Link>
-          </SheetClose>
-        );
-      })}
-    </section>
-  );
-};
+import { signOut, useSession } from 'next-auth/react';
+import NavLinks from './NavLinks';
 
 const MobileNav = () => {
   const session = useSession();
   const userName = session?.data?.user?.name || 'User';
-  const [loading, isLoading] = useState();
 
   return (
     <Sheet>
@@ -107,7 +66,7 @@ const MobileNav = () => {
         </SheetDescription>
         <div className="no-scrollbar flex grow flex-col justify-between overflow-y-auto">
           <SheetClose asChild>
-            <NavContent />
+            <NavLinks />
           </SheetClose>
         </div>
         <div>
@@ -131,7 +90,10 @@ const MobileNav = () => {
             </div>
           ) : (
             <SheetClose asChild>
-              <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none cursor-pointer">
+              <Button
+                className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none cursor-pointer"
+                onClick={() => signOut()}
+              >
                 <span className="primary-text-gradient">LogOut</span>
               </Button>
             </SheetClose>
--- src/components/navigation/navbar/NavLinks.jsx
+++ src/components/navigation/navbar/NavLinks.jsx
@@ -1,7 +1,45 @@
+import { SheetClose } from '@/components/ui/sheet';
+import { sidebarLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  const pathname = usePathname();
+  return (
+    <section className="flex h-full flex-col gap-6">
+      {sidebarLinks.map(item => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? 'primary-gradient rounded-lg text-light-900'
+                  : 'text-dark300_light900'
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? '' : 'invert-colors'}`}
+              />
+              <p className={`${isActive ? 'base-bold' : 'base-medium'}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 };
 
 export default NavLinks;
