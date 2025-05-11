517e6b25af8a48b68202a52a6efb949a9dacdfe9
Siddarth Banerjee <siddarth2801@gmail.com>
2025-05-12 01:17:39 +0530

    added tags for name, votes, answers, views

--- src/components/cards/QuestionCard.jsx
+++ src/components/cards/QuestionCard.jsx
@@ -1,6 +1,9 @@
 import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import React from 'react';
+import DataRenderer from '../DataRenderer';
+import Metric from '../Metric';
+import { formatNumber, getTimeStamp } from '@/lib/utils';
 
 const QuestionCard = ({
   _id,
@@ -17,7 +20,7 @@ const QuestionCard = ({
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {String(createdAt)}
+            {getTimeStamp(createdAt)}
           </span>
           <Link href={ROUTES.QUESTION(_id)}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
@@ -25,8 +28,47 @@ const QuestionCard = ({
             </h3>
           </Link>
         </div>
-        TODO: If user is signed in and the question belongs to that user, edit
-        option should be visible.
+        {/* TODO: If user is signed in and the question belongs to that user, edit
+        or delete option should be visible. */}
+      </div>
+
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map(tag => (
+          <DataRenderer key={tag._id} _id={tag._id} name={tag.name} />
+        ))}
+      </div>
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl="/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimeStamp(createdAt)}`}
+          href={ROUTES.PROFILE(_id)}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+        <Metric
+          imgUrl="/icons/like.svg"
+          alt="upvotes"
+          value={formatNumber(upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/icons/message.svg"
+          alt="message"
+          value={formatNumber(answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/icons/eye.svg"
+          alt="eye"
+          value={formatNumber(views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
       </div>
     </div>
   );
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,43 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+// Using Chatgpt for calculating getTimeStamp
+export const getTimeStamp = createdAt => {
+  const date = new Date(createdAt);
+  const now = new Date();
+
+  const diffMilliseconds = now.getTime() - date.getTime();
+  const diffSeconds = Math.round(diffMilliseconds / 1000);
+  if (diffSeconds < 60) {
+    return `${diffSeconds} seconds ago`;
+  }
+
+  const diffMinutes = Math.round(diffSeconds / 60);
+  if (diffMinutes < 60) {
+    return `${diffMinutes} mins ago`;
+  }
+
+  const diffHours = Math.round(diffMinutes / 60);
+  if (diffHours < 24) {
+    return `${diffHours} hours ago`;
+  }
+
+  const diffDays = Math.round(diffHours / 24);
+
+  return `${diffDays} days ago`;
+};
+
+// format number acccordingly for better number views
+export function formatNumber(number) {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + 'M';
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + 'K';
+  } else {
+    return number.toString();
+  }
 }
