e6b6de3a08bf723bbdb8af0743e5b30e481c07e1
Siddarth Banerjee <siddarth2801@gmail.com>
2025-05-22 02:00:51 +0530

    Tag Details page completed

--- src/app/(root)/ask-question/page.jsx
+++ src/app/(root)/ask-question/page.jsx
@@ -1,7 +1,7 @@
 import QuestionForm from '@/components/forms/QuestionForm';
 import React from 'react';
 
-const AskQuestion = async () => {
+const AskQuestion = () => {
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
